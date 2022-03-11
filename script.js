3a1a21da48cd3fc08b8f014773d2cf44cc3d986d
vov4k <golovach.volodia@yandex.ru>
2022-03-11 18:02:37 +0300

    Init commit

--- script.js
+++ script.js
@@ -6,33 +6,36 @@ let nowShow = document.getElementById("now"); // Берём блок для по
 let divTime = document.getElementById("time"); // Берём блок для показа времени
 
 Date.prototype.addMinutes = function (h) {
-  this.setMinutes(this.getMinutes() + h);
-  return this;
+    this.setMinutes(this.getMinutes() + h);
+    return this;
 }
 
 buttonRun.addEventListener('click', function () {
-  timeMinut = 5400;
-  var date = new Date()
-  startShow.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
-  date.addMinutes(90);
-  endShow.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
+    timeMinut = 5459;
+    var date = new Date()
+    startShow.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
+    date.addMinutes(90);
+    endShow.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
 
-  divTime.style.display = "";
-  buttonRun.style.display = "none";
+    divTime.style.display = "";
+    buttonRun.style.display = "none";
+
+    document.documentElement.requestFullscreen();
 })
 
 timer = setInterval(function () {
-  seconds = timeMinut % 60 // Получаем секунды
-  minutes = timeMinut / 60 % 60 // Получаем минуты
-  hour = timeMinut / 60 / 60 % 60 // Получаем часы
+    seconds = timeMinut % 60 // Получаем секунды
+    minutes = timeMinut / 60 % 60 // Получаем минуты
+    hour = timeMinut / 60 / 60 % 60 // Получаем часы
 
-  if (timeMinut <= 0) {
-    clearInterval(timer);
-    alert("Время закончилось");
-  } else {
-    timerShow.innerHTML = `${Math.trunc(hour)}:${Math.trunc(minutes)}`;
-    var date = new Date()
-    nowShow.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
-  }
-  --timeMinut; // Уменьшаем таймер
+    if (timeMinut <= 0) {
+        clearInterval(timer);
+        alert("Время закончилось");
+    } else {
+        timerShow.innerHTML = `${Math.trunc(hour)}:${Math.trunc(minutes)}`;
+        var date = new Date()
+        nowShow.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
+    }
+    --timeMinut; // Уменьшаем таймер
+
 }, 1000)
